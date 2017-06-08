937ed9c6b85279a416b5a601a433c586a22f4601
sangeetha113 <jsangeetha9316@gmail.com>
2017-06-08 09:52:49 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class alphabet
+{
+    public static void main(String args[])
+    {
+        char ch;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter an Alphabet : ");
+        ch = scan.next().charAt(0);
+		
+        if((ch=='a' && ch=='A') || (ch=='z' && ch=='Z'))
+        {
+            System.out.print("This is an Alphabet");
+        }
+        else
+        {
+            System.out.print("This is not an Alphabet");
+        }
+    }
+}
+
