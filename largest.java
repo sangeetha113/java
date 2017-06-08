0d8065a4c7b790b371bcbf63db85a3828f1fe7d4
sangeetha113 <jsangeetha9316@gmail.com>
2017-06-08 11:25:06 +0530

    Create largest.java

--- largest.java
+++ largest.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 class largest
 {
-public static void main(String[] args) 
-{
-int a,b,c,largest;
-Scanner s=new Scanner(System.in);
-System.out.println("Enter any three numbers: ");
-a=s.nextInt();
-b=s.nextInt();
-c=s.nextInt();
-largest=(a>b)?(a>c?a:c):(b>c?b:c);
-System.out.println("Largest number: "+largest);
-}
+  public static void main(String args[])
+  {
+    int a,b,c,large;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    large=(a>b)?(a>c?a:c):(b>c?b:c);
+    System.out.println(large);
+  }
 }
