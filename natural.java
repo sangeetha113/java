0e743a5e92fc9deb9f0ba3ca6b249b9916d35422
sangeetha113 <jsangeetha9316@gmail.com>
2017-06-08 10:20:25 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class natural
+{
+public static void main(String args[])
+{
+int n,i=1;sum=0;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+d0
+{
+sum=sum+i;
+i=i+1;
+}
+while(i<=n)
+System.out.println("sum of first" +n+ "numbers ="+sum);
+}
+}
+
