9b1341c6f3f026000e654f3c16bcbaa13935e62c
sangeetha113 <jsangeetha9316@gmail.com>
2017-06-13 12:15:40 +0530

    Create Gcd.java

---
+++ Gcd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Gcd
+{
+    public static void main(String args[])
+    {
+        int r;
+        Scanner s=new Scanner(System.in);
+        int n1=s.nextInt();
+        int n2=s.nextInt();
+        int x,y;
+        x=n1;
+        y=n2;
+        while(n2>0)
+        {
+        r=n1%n2;
+            n1=n2;
+            n2=r;
+        }
+            int gcd=n1;
+            
+        
+        System.out.println(gcd+" ");
+    }
+}
